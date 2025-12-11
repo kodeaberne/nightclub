95e2c4a2a8c07ecbb306253b6146451cc38569f4
Mikkel Jørgensen <mikkel@codemonkey.computer>
2025-12-11 12:38:14 +0100

    chore(fonts): add Ubuntu font
    - Added Ubuntu font and removed Next.js defaults.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import Navigation from "@/app/components/global/Navigation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ubuntu.variable} antialiased`}
       >
+        <Navigation />
         {children}
       </body>
     </html>
